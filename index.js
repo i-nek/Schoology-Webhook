e69f4af84b154e27be83b92f80b888eacc6366f0
KuyaKen <ricewitduhspringroll@gmail.com>
2023-09-07 10:33:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const APIController = (function() {
     const DOMAIN         = 'https://lms.lausd.net';
-    const consumerId     = '';
+    const consumerId     = 'efe5a543948026772946115c316323cd05e443b00';
     const consumerSecret = ''
 
     const _getToken = async () => {
-        const result = await fetch(DOMAIN + '/oauth/authorize', {
+        const result = await fetch('https://api.schoology.com/oauth/request_token', {
             method: 'POST',
             headers: {
                 'Authorization': 'Basic ' + btoa(consumerId + ':' + consumerSecret),
-                'Host': 'api.schoology.com',
+                'Host': 'lms.lausd.net',
                 'Content-Type': 'application/json'
             },
             body: 'grant_type=client_credentials'
         });
 
         const data = await result.json();
-        return data.access_token;
+        return data;
     }
-})(APIController);
+    console.log(_getToken());
+})();
