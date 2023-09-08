b764a984d36d5504858a3a5ea9babc4d8c38f8e9
Wicey2050 <92195844+Wicey2050@users.noreply.github.com>
2023-09-07 19:30:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -57,32 +57,3 @@ const APIController = (function() {
     console.log(_getToken());
 })();
 */
-  
-  
-  
-  
-
-
-
-=======
-    const domain         = 'lausdschoology.azurewebsites.net/?&domain=lms.lausd.net';
-    const consumerId     = 'efe5a543948026772946115c316323cd05e443b00';
-    const consumerSecret = 'e9dc645bf33230645546a37fc9d80348'
-
-    const _getToken = async () => {
-        const result = await fetch('https://api.schoology.com/oauth/request_token', {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Basic ' + btoa(consumerId + ':' + consumerSecret),
-                'Host': domain,
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: 'grant_type=authorization_code'
-        });
-
-        const data = await result.json();
-        return data;
-    }
-    console.log('OAuth Token: ' + _getToken());
-})();
->>>>>>> f913c70f0a9cb4e8cd0ec123ee628bff3f405066
